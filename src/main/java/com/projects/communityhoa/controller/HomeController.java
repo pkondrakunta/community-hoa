c444d7cb19fc06c9749fa4c1b4d7f61a3d3b5915
pkondrakunta <pragnyakondrakunta@gmail.com>
2023-11-28 16:57:39 -0500

    test commit

--- src/main/java/com/projects/communityhoa/controller/HomeController.java
+++ src/main/java/com/projects/communityhoa/controller/HomeController.java
@@ -49,7 +49,7 @@ public class HomeController {
 		return "search-member";
 	}
 
-	@PostMapping("/search-member")
+	@PostMapping("/search-mem")
 	public String searchMember(@ModelAttribute("member-text") String search_text, SessionStatus status,
 			HttpServletRequest request) {
 		List<Member> searchMemberResults = memberService.searchMembers(search_text);
