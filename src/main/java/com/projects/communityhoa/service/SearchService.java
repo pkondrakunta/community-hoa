28d7f4d72c026a5d72b64f034e9467af524f21dc
pkondrakunta <pragnyakondrakunta@gmail.com>
2023-11-28 13:08:07 -0500

    added searchMember() in SearchService

--- src/main/java/com/projects/communityhoa/service/SearchService.java
+++ src/main/java/com/projects/communityhoa/service/SearchService.java
@@ -11,6 +11,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.projects.communityhoa.model.Invoice;
+import com.projects.communityhoa.model.Member;
 
 public class SearchService {
 
@@ -62,6 +63,45 @@ public class SearchService {
 		} catch (SQLException ex) {
 			Logger.getLogger(SearchService.class.getName()).log(Level.SEVERE, null, ex);
 			return new ArrayList<Invoice>();
+		}
+	}
+	
+	public List<Member> searchMember(String search_text) {
+
+		try {
+			String query = "SELECT * FROM members WHERE '%?%' IN member_id";
+			PreparedStatement statement = conn.prepareStatement(query);
+
+			statement.setString(1, search_text);
+			statement.setString(2, search_text);
+
+			ResultSet resultSet = statement.executeQuery(query);
+
+			// Loop through the ResultSet
+			List<Member> memberList = new ArrayList<Member>();
+
+			while (resultSet.next()) {
+
+				 Member memObj = new Member();
+
+				 memObj.setMemberID(resultSet.getString("member_id"));
+				 memObj.setFirstName(resultSet.getString("first_name"));
+				 memObj.setLastName(resultSet.getString("last_name"));
+				 memObj.setUnit(resultSet.getString("unit"));
+				 memObj.setUnitType(resultSet.getString("unit_type"));
+				 memObj.setPhone(resultSet.getInt("phone"));
+				 memObj.setYearly(resultSet.getBoolean("yearly"));
+				 memObj.setSubscriptionExpiry(resultSet.getDate("subscription_expiry"));
+				 memObj.setLastPaid(resultSet.getDate("last_paid"));
+
+				 memberList.add(memObj);
+			}
+
+			return memberList;
+
+		} catch (SQLException ex) {
+			Logger.getLogger(SearchService.class.getName()).log(Level.SEVERE, null, ex);
+			return new ArrayList<Member>();
 		}
 	}
 
