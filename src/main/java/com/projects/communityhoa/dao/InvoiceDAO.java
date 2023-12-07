a4ddf432f324fafb596525da90351c426183fc9f
pkondrakunta <pragnyakondrakunta@gmail.com>
2023-12-07 14:31:14 -0500

    added InvoiceDAO

---
+++ src/main/java/com/projects/communityhoa/dao/InvoiceDAO.java
@@ -0,0 +1,22 @@
+package com.projects.communityhoa.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.projects.communityhoa.model.Invoice;
+
+@Repository
+public interface InvoiceDAO {
+	public void save(Invoice i);
+
+	// No updates can be made to an invoice. A new transaction has to be created
+	// public void update(Invoice i);
+
+	// No deletes can be made to an invoice. A new transaction has to be created for a refund.
+	// public void delete(Invoice i);
+
+	public Invoice getInvoiceById(String Id);
+
+	public List<Invoice> getAllInvoices();
+}
